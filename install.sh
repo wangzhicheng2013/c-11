0772e3cd413246e324ca02b08b6c31cda3ece520
root <root@ubuntu.ubuntu-domain>
2018-11-16 22:08:10 +0800

    42 commit

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #echo "# c-11" >> README.md
 #git init
+find ./* -name "*.o" | xargs rm -rf
 git add *
 git commit -m "42 commit"
 #git remote add origin https://github.com/wangzhicheng2013/c-11.git
