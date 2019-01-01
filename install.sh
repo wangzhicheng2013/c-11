4198b579f9e75986cbd3b2b039cc8e7cda2ad3fb
root <root@ubuntu.ubuntu-domain>
2019-01-01 21:55:09 +0800

    63 commit

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 #git init
 find ./* -name "*.o" | xargs rm -rf
 git add *
-git commit -m "62 commit"
+git commit -m "63 commit"
 #git remote add origin https://github.com/wangzhicheng2013/c-11.git
 git push -u origin master
