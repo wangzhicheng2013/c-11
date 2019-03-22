45917a4c5d61ef134522a81c2dc3f86620a078ed
root <root@ubuntu.ubuntu-domain>
2019-03-22 22:02:48 +0800

    100 commit

---
+++ linux/examples/example17.cpp
@@ -0,0 +1,36 @@
+#include <stdint.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int x = 13;
+int y = 13;
+const int LOOP = 1000000;
+int fun0()
+{
+    int last = 1;
+    for (int i = 1;i <= y;i++)
+    {
+        last = last * x % 1000;
+    }
+    return last;
+}
+int fun1()
+{
+    uint64_t t = pow(x, y);
+    return t % 1000;
+}
+int main()
+{
+    // 0.1453s
+    /*for (int i = 0;i < LOOP;i++)
+    {
+        fun0();
+    }*/
+    // 0.191s
+    for (int i = 0;i < LOOP;i++)
+    {
+        fun1();
+    }
+    
+    return 0;
+}
