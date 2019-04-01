ffa4077bbe46880a1da592eef96a79f6a8804795
root <root@ubuntu.ubuntu-domain>
2019-04-01 22:02:06 +0800

    103 commit

---
+++ linux/examples/example20.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int *p = 0;
+    *p = 10;
+    
+    return 0;
+}
--- linux/examples/make.sh
+++ linux/examples/make.sh
@@ -1,1 +1,1 @@
-g++ -std=c++14 -g -o test example19.cpp -pthread
+g++ -std=c++14 -g -o test example20.cpp -pthread
