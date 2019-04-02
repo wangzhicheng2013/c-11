11d43c21d6f65585163c72489017905308477c0a
root <root@ubuntu.ubuntu-domain>
2019-04-02 22:02:47 +0800

    104 commit

---
+++ linux/examples/example21.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    printf("hello world");
+    while (true)
+    {
+        sleep(10);
+    }
+    return 0;
+}
--- linux/examples/make.sh
+++ linux/examples/make.sh
@@ -1,1 +1,1 @@
-g++ -std=c++14 -g -o test example20.cpp -pthread
+g++ -std=c++14 -g -o test example21.cpp -pthread
